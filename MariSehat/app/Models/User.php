2d950812a5e48ca7671304a31dc9a6f348660056
William Limanto <limantowilliam05@gmail.com>
2021-12-09 19:45:45 +0700

    Update User.php

--- MariSehat/app/Models/User.php
+++ MariSehat/app/Models/User.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 
+as
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
