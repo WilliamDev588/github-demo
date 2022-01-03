ddb1adf3d015aafee4bba6bd0aacc0986e476ad8
nicoarifin <nico.arifin@binus.ac.id>
2022-01-03 21:21:34 +0700

    bagusin about dikit

--- MariSehat/resources/views/user/about.blade.php
+++ MariSehat/resources/views/user/about.blade.php
@@ -1,19 +1,57 @@
 
 @extends('layouts.main')
 @section('container')
-          <div id='white_Box'>
-            <h3 style="text-align:center;">MariSehat</h3>
-            
 
-                    <br>A very warm welcome to you ! At MariSehat we provide the very best in Food Calculator, Fitness Gear and other Healthy Things. </br>
-                    <br> MariSehat was founded with the idea of being able to give Everyone the best Food Calculator, Fitness Gear and other Healthy Things possible. If you are like us and you have trouble counting your daily macros, need help counting them, then count on us ! Don't worry we've got you covered! It is a platform that provides a food calculator as its's main feature so you can improve your lifestyle </br>
-                    <br> At MariSehat we are dedicated to: </br>
-                    <br>  Make Your Life Easier </br>
-                    <br> Make Your Life Healthier </br>
-                    <br>  Make Your Life Better </br>
-                    <br> We are here to help! If you need any help at all please don't hesitate to contact us. Thank you for your interest in MariSehat  From all of us at MariSehat </br>
 
-        
-          </div>
+
+<!-- Page Header Start -->
+<div class="page-header">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h2>About Us</h2>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Page Header End -->
+
+
+              <!-- About Start -->
+
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-lg-5 col-md-7">
+                        <div class="about-img">
+                            <img src="aboutz/people-eating-healthy-exercising-regularly_53876-64671.jpg" alt="Image" style="width:500px;">
+                        </div>
+                    </div>
+                    <div class="col-lg-7 col-md-6">
+                        <div class="section-header text-left">
+
+                            <h2>Welcome to MariSehat</h2>
+                        </div>
+                        <div class="about-text">
+                            <p>
+                                A very warm welcome to you ! At MariSehat we provide the very best in Food Calculator, Fitness Gear and other Healthy Things.
+                            </p>
+                            <p>
+                                MariSehat was founded with the idea of being able to give Everyone the best Food Calculator, Fitness Gear and other Healthy Things possible. If you are like us and you have trouble counting your daily macros, need help counting them, then count on us ! Don't worry we've got you covered! It is a platform that provides a food calculator as its's main feature so you can improve your lifestyle
+                            </p>
+                            <p>
+                                We are here to help! If you need any help at all please don't hesitate to contact us. Thank you for your interest in MariSehat  From all of us at MariSehat
+                            </p>
+                            <br> Lets Make Your Life Better! </br>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        <!-- About End -->
+
+
 
           @endsection
+
+
