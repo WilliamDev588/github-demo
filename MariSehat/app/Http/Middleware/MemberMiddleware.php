d0f9d60b40711bbbebd24265e19893e0305cd442
RaymonddC <raymondchrisandy@gmail.com>
2022-01-05 16:17:58 +0700

    middle dimana

--- MariSehat/app/Http/Middleware/MemberMiddleware.php
+++ MariSehat/app/Http/Middleware/MemberMiddleware.php
@@ -5,7 +5,7 @@ namespace App\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 
-class AdminMiddleware
+class MemberMiddleware
 {
     /**
      * Handle an incoming request.
@@ -16,7 +16,7 @@ class AdminMiddleware
      */
     public function handle(Request $request, Closure $next)
     {
-        if(!auth()->check() || auth()->user()->role!='admin'){
+        if(!auth()->check() || auth()->user()->role!='customer'){
             return redirect()->back();
         }
         return $next($request);
